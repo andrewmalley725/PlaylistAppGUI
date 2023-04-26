0782e15b105cacf2072e2cd694fdb6b85d70df8b
andrewmalley725 <andrew.alley99@gmail.com>
2023-04-25 19:52:50 -0600

    added login interface

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -2,10 +2,25 @@ import React from "react";
 
 export default function HomePage()
 {
+function logout() {
+    localStorage.clear();
+    window.location.reload();
+}
+
     return(
         <div>
             {
-                localStorage.getItem('userId') ? <h1>Welcome {localStorage.getItem('username')}!</h1> : <h1>Generic HomePage</h1>
+                localStorage.getItem('userId') ? 
+                <div>
+                    <h1>Welcome {localStorage.getItem('username')}!</h1>
+                    <button type="button" className="btn btn-primary" onClick={logout}>Logout</button>
+                </div>
+                
+                : 
+                <div>
+                    <h1>Generic HomePage</h1>
+                    <a href="/login" className="btn btn-primary">Login</a>
+                </div>
             }
         </div>
     )
